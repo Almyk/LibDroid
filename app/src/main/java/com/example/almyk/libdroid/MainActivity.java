12346a50c68b32830551af9181768ee3363a847f
almyk <hansentommy92@gmail.com>
2019-01-13 10:13:30 +0900

    Add skeleton for search result activity

--- app/src/main/java/com/example/almyk/libdroid/MainActivity.java
+++ app/src/main/java/com/example/almyk/libdroid/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.almyk.libdroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private ImageButton mSearchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSearchButton = findViewById(R.id.ib_search);
+        mSearchButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int viewID = v.getId();
+        switch(viewID){
+            case R.id.ib_search:
+//              TODO: make a query to libgen
+                Intent intent = new Intent(this, SearchResultActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
