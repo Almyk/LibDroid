970a2ce7f067d7c9ac1793d5866d522dfcb7c3a7
almyk <hansentommy92@gmail.com>
2019-01-17 15:42:41 +0900

    Add functionality to show which language is chosen in settings

--- app/src/main/java/com/example/almyk/libdroid/MainActivity.java
+++ app/src/main/java/com/example/almyk/libdroid/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.almyk.libdroid;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
--- app/src/main/java/com/example/almyk/libdroid/SettingsFragment.java
+++ app/src/main/java/com/example/almyk/libdroid/SettingsFragment.java
@@ -1,12 +1,72 @@
 package com.example.almyk.libdroid;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v14.preference.MultiSelectListPreference;
+import android.support.v7.preference.EditTextPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceManager;
+import android.support.v7.preference.PreferenceScreen;
+import android.util.Log;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+import java.util.Arrays;
+import java.util.Map;
+
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private SharedPreferences mSharedPreferences;
 
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
         addPreferencesFromResource(R.xml.search_settings);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        mSharedPreferences = getPreferenceManager().getSharedPreferences();
+        mSharedPreferences.registerOnSharedPreferenceChangeListener(this);
+
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        for(int i = 0; i < preferenceScreen.getPreferenceCount(); i++){
+            setSummary(getPreferenceScreen().getPreference(i));
+        }
+    }
+
+    @Override
+    public void onPause() {
+        mSharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
+        super.onPause();
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = getPreferenceScreen().findPreference(key);
+        setSummary(preference);
+    }
+
+    private void setSummary(Preference pref) {
+        if (pref instanceof EditTextPreference) {
+            updateSummary((EditTextPreference) pref);
+        } else if (pref instanceof ListPreference) {
+            updateSummary((ListPreference) pref);
+        } else if (pref instanceof MultiSelectListPreference) {
+            updateSummary((MultiSelectListPreference) pref);
+        }
+    }
+
+    private void updateSummary(MultiSelectListPreference pref) {
+        pref.setSummary(Arrays.toString(pref.getValues().toArray()));
+    }
+
+    private void updateSummary(ListPreference pref) {
+        pref.setSummary(pref.getValue());
+    }
+
+    private void updateSummary(EditTextPreference preference) {
+        preference.setSummary(preference.getText());
     }
 }
